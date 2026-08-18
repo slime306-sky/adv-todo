52263ec2c1cad81b671ce0466db03d0cfc0a1ff7
slime <smileslime271@gmail.com>
2026-08-18 08:58:25 +0000

    mistake removed complete subtasks

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1559,6 +1559,15 @@ def get_task_progress(
         or 0
     )
 
+    completed_subtasks = (
+        db.query(func.count(SubTask.id))
+        .filter(SubTask.task_id == task_id)
+        .filter(SubTask.status == SubTaskStatus.complete.value)
+        .filter(~SubTask.id.in_(excluded_sub_task_ids))
+        .scalar()
+        or 0
+    )
+
     completed_weightage = (
         db.query(func.coalesce(func.sum(SubTask.weightage_priority), 0))
         .filter(SubTask.task_id == task_id)
